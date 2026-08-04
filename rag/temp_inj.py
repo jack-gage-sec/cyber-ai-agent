481d6498acabdecb0cfcb70298995361df9cefda
jack-gage-sec <jackthetechguy10137@gmail.com>
2026-08-04 00:53:12 -0400

    Refactor path handling in temp_inj.py

--- rag/temp_inj.py
+++ rag/temp_inj.py
@@ -1,7 +1,10 @@
 import os
 import sys
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "D:\Backup Files\Experiments\Compliance Evidence Pipeline"))
+from pathlib import Path
+
+dir_above_cep = Path(__file__).resolve().parents[2]
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), dir_above_cep))
 
 external_path = os.path.join(parent_dir, "Compliance-AI")
 sys.path.append(external_path)
